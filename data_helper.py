17370e4e05fb0b0e1c43a9306c3522f1aa87e383
angrypark <angrypark@Sungnamui-iMac.local>
2018-05-21 16:43:18 +0900

    made train.py, modified data_helper

--- data_helper.py
+++ data_helper.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 from tflearn.data_utils import pad_sequences
 
 PAD_ID = 0
@@ -27,6 +28,24 @@ def split_data(data):
     lines = [line.split('\t')[0] for line in data]
     labels = [line.split('\t')[1] for line in data]
     return lines, labels
+
+def batch_iter(data, batch_size, num_epochs, shuffle=False):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((data_size-1)/batch_size)+1
+    for epoch in range(num_epochs):
+        if shuffle:
+            shuffle_indices = np.random.permutate(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_idx = batch_num*batch_size
+            end_idx = min((batch_num+1)*batch_size, data_size)
+            yield shuffled_data[start_idx:end_idx]
 
 
 class Preprocessor:
--- train.py
+++ train.py
@@ -8,8 +8,9 @@ import datetime
 from tensorflow.contrib import learn
 
 from . import tokenizers, normalizers, vectorizers
-from .data_helper import Preprocessor, load_data, split_data
+from .data_helper import Preprocessor, load_data, split_data, batch_iter
 import models
+from models import TextCNN
 
 
 # Parameters
@@ -46,6 +47,8 @@ args.add_argument("--batch_size", type=int, default=64)
 args.add_argument("--num_epochs", type=int, default=200)
 args.add_argument("--evaluate_every", type=int, default=1)
 args.add_argument("--checkpoint_every", type=int, default=1)
+args.add_argument("--num_checkpoints", type=int, default=30)
+args.add_argument("--shuffle", type=bool, default=True)
 
 # Misc parameters
 args.add_argument("--allow_soft_replacement", type=bool, default=True)
@@ -59,8 +62,7 @@ logger.info("Arguments : {}".format(config))
 def main(_):
     # Load model, normalizer and tokenizer
     # ==================================================
-    if config.model == 'TextCNN':
-        Model = TextCNN.TextCNN
+    Model = getattr(models, config.model)
     Normalizer = getattr(normalizers, config.normalizer)
     Tokenizer = getattr(tokenizers, config.tokenizer)
 
@@ -82,12 +84,18 @@ def main(_):
     preprocessor = Preprocessor(config, normalizer, tokenizer, vectorizer)
     train_data, val_data = preprocessor.preprocess(train_data), preprocessor.preprocess(val_data)
 
-    # Create Session
+    # Set device setting
     device_config = tf.ConfigProto()
+    device_config.allow_soft_placement = config.allow_soft_placement
+    device_config.log_device_placement = config.log_device_placement
     device_config.gpu_options.allow_growth = True
+
+    # Training
+    # ==================================================
     with tf.Session(config=device_config) as sess:
         # Create model
-        model = Model(config, 3)
+        num_classes = np.unique(train_labels)
+        model = Model(config, num_classes)
 
         # Create Saver
         saver = tf.train.Saver()
@@ -98,7 +106,96 @@ def main(_):
             print("Initializing Variables")
             sess.run(tf.global_variables_initializer())
             if config.pretrained_embed_dir:
-                load_pretrained_embedding(sess)
+                embedding = load_pretrained_embedding(sess, config.pretrained_model_dir)
+                tf.assign(model.Embedding, embedding)
+
+        # Define training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(model.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # Keep track of gradient values and sparsity
+        grad_summaries = list()
+        for g, v in grads_and_vars:
+            if g is not None:
+                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.summary.merge(grad_summaries)
+
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        print("Writing to {}\n".format(out_dir))
+
+        # Loss and accuracy summaries
+        loss_summary = tf.summary.scalar("loss", model.loss)
+        acc_summary = tf.summary.scalar("accuracy", model.accuracy)
+
+        # Train summaries
+        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
+        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+        # Validation summaries
+        val_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
+        val_summary_dir = os.path.join(out_dir, "summaries", "val")
+        val_summary_writed = tf.summary.FileWriter(val_summary_dir, sess.graph)
+
+        # Checkpoint directory
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=config.num_checkpoints)
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
+            feed_dict = {model.input_x: x_batch,
+                         model.input_y: y_batch,
+                         model.dropout_keep_prob: config.dropout_keep_prob}
+            _, step, summaries, loss, accuracy = sess.run([train_op, global_step, train_summary_op, model.loss,
+                                                           model.accuracy],
+                                                          feed_dict=feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            train_summary_writer.add_summary(summaries, step)
+
+        def val_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a validation set
+            """
+            feed_dict = {
+                model.input_x: x_batch,
+                model.input_y: y_batch,
+                model.dropout_keep_prob: 1.0
+            }
+            step, summaries, loss, accuracy = sess.run([global_step, val_summary_op, model.loss, model.accuracy],
+                                                       feed_dict=feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            if writer:
+                writer.add_summary(summaries, step)
+
+        # Generate batches
+        batches = batch_iter(list(zip(train_data, train_labels)), config.batch_size, config.num_epochs, config.shuffle)
+
+        # Training loop for each batch
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            train_step(x_batch, y_batch)
+            current_step = tf.train.global_step(sess, global_step)
+            if current_step % config.evaluate_every == 0:
+                print("\nEvaluation : ")
+                val_step(val_data, val_labels)
+                print("")
+            if current_step % config.checkpoint_every == 0:
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print("Saved model checkpoint to {}\n".format(path))
 
 
 def load_pretrained_embedding(shape, pretrained_embed_path, trainable=True):
@@ -108,4 +205,3 @@ def load_pretrained_embedding(shape, pretrained_embed_path, trainable=True):
     with tf.variable_scope("embedding"):
         embedding = tf.get_variable(name='pretrained_embedding',shape=shape, initializer=initializer, trainable=trainable)
     return embedding
-
